0aaa2ed859604552001c78c52c1e8d690482177c
Rodrigo Webler <rwebler@gmail.com>
2015-11-22 16:42:54 -0200

    Placing pins

--- VirtualTourist2/ViewController.swift
+++ VirtualTourist2/ViewController.swift
@@ -7,17 +7,93 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var longPressGestureRecognizer: UILongPressGestureRecognizer?
+    
+    var pins = [MKPointAnnotation]()
+    var currentPin = MKPointAnnotation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        reload()
+    }
+    
+    func reload() {
+    }
+    
+    func displayError(errorString: String?) {
+        if let errorString = errorString {
+            
+            //display alert with error message
+            let alert = UIAlertController(title: "Map Loading Failed", message: errorString, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
+    
+    //Map Delegate Functions
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        print("In viewForAnnotation")
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.pinTintColor = MKPinAnnotationView.purplePinColor()
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        
+        print("In didSelectAnnotationView")
+    }
+    
+    // Long Press Gesture Action
+    @IBAction func longPressed(sender: UILongPressGestureRecognizer)
+    {
+        print("longpressed")
+        let point = sender.locationInView(mapView)
+        let coordinate = mapView.convertPoint(point , toCoordinateFromView: mapView)
+        
+        switch (sender.state) {
+        case .Began:
+            print("Began")
+            print(coordinate)
+            let pin = MKPointAnnotation()
+            currentPin = pin
+            currentPin.coordinate = coordinate
+            mapView.addAnnotation(currentPin)
+        case .Changed:
+            print("Changed")
+            print(coordinate)
+            currentPin.coordinate = coordinate
+        case .Ended:
+            print("Ended")
+            currentPin.coordinate = coordinate
+            pins.append(currentPin)
+        default:
+            print(sender.state)
+            return
+        }
     }
 
 
